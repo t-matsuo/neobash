1ae9e936f1e339d03437ec50e8ea47a205ee40fc
MATSUO Takatoshi <matsuo.tak@gmail.com>
2025-10-25 01:23:51 +0900

    mattermost/mattermost.sh: add mattermost::post_msg()

--- lib/mattermost/mattermost.sh
+++ lib/mattermost/mattermost.sh
@@ -121,6 +121,91 @@ mattermost::webhook_post() {
     fi
 }
 
+# @description post message
+# @stdout API response (json)
+# @stderr Error and debug message.
+# @option --message <vahlue> (string)(required): Message.
+# @option --token <token> (string)(required): token.
+# @option --host <value> (string)(required): Mattermost URL such as https://localhost:8065
+# @option --ch <value> (string)(required): Mattermost channel ID
+# @option --insecure (optional): Ignore certificate errors.
+# @option --verbose (optional): Verbose log.
+# @exitcode 0 If successfull.
+# @exitcode 1 If failed.
+mattermost::post_msg() {
+    local MESSAGE=""
+    local ESCAPED_MESSAGE=""
+    local -r API_PATH="api/v4/posts"
+    local CURL_OPTIONS=""
+    local curl_rc=""
+    local STDOUT=""
+    local STDERR=""
+    local POST_DATA=""
+    local POST_DATA_FOR_FILES=""
+    local file_id=""
+
+    core::arg::init_local
+    core::arg::add_option -l "MESSAGE" -o "--message" -r "true" -h "post message"
+    core::arg::add_option -l "TOKEN" -o "--token" -r "true" -h "token"
+    core::arg::add_option -l "HOST" -o "--host" -r "true" -h "mattermost host such as https://localhost:8065"
+    core::arg::add_option -l "CH" -o "--ch" -r "true" -h "The ID of the channel that this file will be uploaded to"
+    core::arg::add_option -l "FILE_IDS" -o "--files" -r "false" -d "" -h "uploaded file IDs. Separator is space."
+    core::arg::add_option -l "INSECURE" -o "--insecure" -r "false" -t "bool" -s "true" -h "ignore certificate errors"
+    core::arg::add_option -l "VERBOSE" -o "--verbose" -r "false" -t "bool" -s "true" -h "verbose curl log"
+    core::arg::parse "$@"
+
+    MESSAGE="${ARGS[MESSAGE]}"
+    [[ "${ARGS[INSECURE]}" == "true" ]] && CURL_OPTIONS="$CURL_OPTIONS --insecure"
+    [[ "${ARGS[VERBOSE]}" == "true" ]] && CURL_OPTIONS="$CURL_OPTIONS --verbose"
+
+    # escape double quote
+    ESCAPED_MESSAGE="${MESSAGE//\"/\\\"}"
+
+    # remove control characters
+    ESCAPED_MESSAGE="${ESCAPED_MESSAGE//[]/}"
+
+    # custructing file_ids value
+    if [[ -z "${ARGS[FILE_IDS]}" ]]; then
+        POST_DATA_FOR_FILES="\"\""
+    else
+        POST_DATA_FOR_FILES="\""
+        for file_ids in ${ARGS[FILE_IDS]}; do
+            POST_DATA_FOR_FILES="$POST_DATA_FOR_FILES\", \"$file_ids"
+        done
+        POST_DATA_FOR_FILES="$POST_DATA_FOR_FILES\""
+    fi
+
+    POST_DATA='{
+      "channel_id": "'"${ARGS[CH]}"'",
+      "message": "'"$ESCAPED_MESSAGE"'",
+      "file_ids": [
+        '"$POST_DATA_FOR_FILES"'
+      ]
+    }'
+
+    POST_DATA="${POST_DATA//$'\n'/}"
+
+    core::log::debug "CURL_OPTIONS=$CURL_OPTIONS"
+    curl::enable_fail
+    if [[ "${MATTERMOST_POST}" == "true" ]]; then
+        util::cmd::exec --stdout STDOUT --stderr STDERR -- curl::post_json $CURL_OPTIONS \
+            --header "Accept: application/json" \
+            --header "Authorization: Bearer ${ARGS[TOKEN]}" \
+            --data "$POST_DATA" \
+            ${ARGS[HOST]}/${API_PATH}
+        curl_rc=$?
+        if [[ $curl_rc -ne 0 ]]; then
+            core::log::error "post message failed rc=$curl_rc channel_id=${ARGS[CH]} msg=\"$ESCAPED_MESSAGE\" CURL_ERROR_LOG=\"$STDERR\""
+            return 1
+        fi
+        core::log::debug "post message successed: rc=$curl_rc channel_id=${ARGS[CH]} msg=\"$ESCAPED_MESSAGE\" CURL_ERROR_LOG=\"$STDERR\""
+        echo "$STDOUT"
+    else
+        core::log::info "post message skipped: channel_id=${ARGS[CH]} msg=\"$ESCAPED_MESSAGE\""
+    fi
+    return 0
+}
+
 # @description Upload a file to mattermost using token. NOTE: Incoming webhook does not support uploading.
 # @stdout API response (json)
 # @stderr Error and debug message.
