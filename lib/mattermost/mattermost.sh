082c4f21a2b83b740aa4f7e5ff7d39d671100608
MATSUO Takatoshi <matsuo.tak@gmail.com>
2025-10-24 23:31:36 +0900

    mattermost/mattermost.sh: add mattermost::upload_file()

--- lib/mattermost/mattermost.sh
+++ lib/mattermost/mattermost.sh
@@ -13,6 +13,7 @@
 
 nb::require "core/log.sh core/arg.sh"
 nb::import "curl/curl.sh"
+nb::import "util/cmd.sh"
 nb::command_check "curl"
 
 MATTERMOST_POST="true"
@@ -118,4 +119,59 @@ mattermost::post() {
     else
         core::log::info "mattermost post skipped: POST_MESSAGE=\"$ESCAPED_MESSAGE\""
     fi
+}
+
+# @description Upload a file to mattermost using token. NOTE: Incoming webhook does not support uploading.
+# @stdout API response (json)
+# @stderr Error and debug message.
+# @option --file <file> (string)(required): file.
+# @option --token <token> (string)(required): token.
+# @option --host <value> (string)(required): Mattermost URL such as https://localhost:8065
+# @option --ch <value> (string)(required): Mattermost channel ID
+# @option --insecure (optional): Ignore certificate errors.
+# @option --verbose (optional): Verbose log.
+# @exitcode 0 If successfull.
+# @exitcode 1 If failed.
+mattermost::upload_file() {
+    local FILE
+    local -r API_PATH="api/v4/files"
+    local CURL_OPTIONS=""
+    local curl_rc=""
+    local STDOUT=""
+    local STDERR=""
+
+    core::arg::init_local
+    core::arg::add_option -l "FILE" -o "--file" -r "true" -h "post message"
+    core::arg::add_option -l "TOKEN" -o "--token" -r "true" -h "token"
+    core::arg::add_option -l "HOST" -o "--host" -r "true" -h "mattermost host such as https://localhost:8065"
+    core::arg::add_option -l "CH" -o "--ch" -r "true" -h "The ID of the channel that this file will be uploaded to"
+    core::arg::add_option -l "INSECURE" -o "--insecure" -r "false" -t "bool" -s "true" -h "ignore certificate errors"
+    core::arg::add_option -l "VERBOSE" -o "--verbose" -r "false" -t "bool" -s "true" -h "verbose curl log"
+    core::arg::parse "$@"
+
+    FILE="${ARGS[FILE]}"
+    [[ "${ARGS[INSECURE]}" == "true" ]] && CURL_OPTIONS="$CURL_OPTIONS --insecure"
+    [[ "${ARGS[VERBOSE]}" == "true" ]] && CURL_OPTIONS="$CURL_OPTIONS --verbose"
+
+    core::log::debug "CURL_OPTIONS=$CURL_OPTIONS"
+    curl::enable_fail
+    if [[ "${MATTERMOST_POST}" == "true" ]]; then
+        util::cmd::exec --stdout STDOUT --stderr STDERR -- curl::post $CURL_OPTIONS \
+            --header "Accept: application/json" \
+            --header "Authorization: Bearer ${ARGS[TOKEN]}" \
+            --header "Content-Type: multipart/form-data" \
+            -F "channel_id=${ARGS[CH]}" \
+            -F "files=@${ARGS[FILE]}" \
+            ${ARGS[HOST]}/${API_PATH}
+        curl_rc=$?
+        if [[ $curl_rc -ne 0 ]]; then
+            core::log::error "file uploading failed rc=$curl_rc channel_id=${ARGS[CH]} file=${ARGS[FILE]} CURL_ERROR_LOG=\"$STDERR\""
+            return 1
+        fi
+        core::log::debug "file uploadig successed: rc=$curl_rc channel_id=${ARGS[CH]} file=${ARGS[FILE]} CURL_ERROR_LOG=\"$STDERR\""
+        echo "$STDOUT"
+    else
+        core::log::info "uploading is skipped: channel_id=${ARGS[CH]} file=${ARGS[FILE]}"
+    fi
+    return 0
 }
